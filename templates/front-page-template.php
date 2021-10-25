48fd51dc06210f8ef4da2b43295df4eb7486b9c3
steelbridge-io <chris@steelbridge.io>
2021-10-24 21:27:49 -0700

    Added mega menu

---
+++ templates/front-page-template.php
@@ -0,0 +1,12 @@
+<?php
+	/**
+	 * Template Name: Front Page
+	 *
+	 * @package      Bootstrap for Genesis
+	 * @since        1.0
+	 * @link         http://wellington-studio.com
+	 * @author       Wellington Studio
+	 * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
+	 *
+	 */
+	genesis();
